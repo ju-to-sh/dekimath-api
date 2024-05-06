c0b9aeb3608df75c4ce313142f95f9ba306eac3a
ju-to-sh <088t3748@gmail.com>
2024-05-06 10:11:21 +0900

    数字を返すAPIを作成

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,6 +1,6 @@
 class NumbersController < ApplicationController
   def index
-    @numbers = Number.question_range_of_numbers(1,50)
+    @numbers = Number.question_range_of_numbers(1,20)
     render json: @numbers
   end
 end
